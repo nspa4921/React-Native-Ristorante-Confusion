138b90627e3426531654dcb547735dae417d3708
Nemanja Spasic <nemo.spaske@live.com>
2020-05-07 23:18:47 +0200

    Initial Setup

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const store = ConfigureStore();
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    );
+  } 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/MenuComponent.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import { View, FlatList } from 'react-native';
+import { Tile } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+      dishes: state.dishes
+    }
+  }
+
+class Menu extends Component {
+
+    
+    render () {
+    
+        const renderMenuItem = ({item, index}) => {
+
+            return (
+                <Tile
+                key={index}
+                title={item.name}
+                caption={item.description}
+                featured
+                onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                imageSrc={{ uri: baseUrl + item.image}}
+                />
+            );
+        };
+
+        const { navigate } = this.props.navigation;
+
+        return (
+            <FlatList 
+                    data={this.props.dishes.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}
+                    />
+        );
+    }
+    
+}
+
+export default connect(mapStateToProps)(Menu);
