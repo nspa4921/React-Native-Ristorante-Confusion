fb2e7388e29eba363c891ed122718a0aff3cfe7e
Nemanja Spasic <nemo.spaske@live.com>
2020-05-15 11:15:53 +0200

    Gesture Part 1

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, FlatList, Modal, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
 import { Card, Icon, Input, Rating } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -20,9 +20,41 @@ const mapDispatchToProps = dispatch => ({
 function RenderDish({ dish, favorite, markFavorite, openCommentForm
 }) {
 
-  if (dish != null) {
-    return (
-      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+  const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+    if ( dx < -200 )
+        return true;
+    else
+        return false;
+}
+
+const panResponder = PanResponder.create({
+  onStartShouldSetPanResponder: (e, gestureState) => {
+      return true;
+  },
+  onPanResponderEnd: (e, gestureState) => {
+      console.log("pan responder end", gestureState);
+      if (recognizeDrag(gestureState))
+          Alert.alert(
+              'Add Favorite',
+              'Are you sure you wish to add ' + dish.name + ' to favorite?',
+              [
+              {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+              {text: 'OK', onPress: () => favorite ? console.log('Already favorited') : markFavorite()},
+              ],
+              { cancelable: false }
+          );
+
+      return true;
+  }
+})
+
+if (dish != null) {
+    return(
+      <Animatable.View
+      animation="fadeInDown"
+      duration={2000}
+      {...panResponder.panHandlers}
+    >
             <Card
             featuredTitle={dish.name}
             image={{uri: baseUrl + dish.image}}>
