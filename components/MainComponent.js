9ecb600c344be3e765bb39d9cde9a1a44d6cde96
Nemanja Spasic <nemo.spaske@live.com>
2020-05-19 15:26:31 +0200

    Picking an Image

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -133,20 +133,21 @@ const FavoritesNavigator = createStackNavigator({
 })
 
 const LoginNavigator = createStackNavigator({
-    Login: { screen: Login }
-  }, {
-  navigationOptions: ({ navigation }) => ({
-    headerStyle: {
-        backgroundColor: "#512DA8"
-    },
-    headerTitleStyle: {
-        color: "#fff"            
-    },
-    headerTintColor: "#fff",
-    headerLeft: <Icon name="menu" size={24}
-      iconStyle={{ color: 'white' }} 
-      onPress={ () => navigation.toggleDrawer() } />    
-  })
+  Login: Login
+}, {
+navigationOptions: ({ navigation }) => ({
+  headerStyle: {
+      backgroundColor: "#512DA8"
+  },
+  headerTitleStyle: {
+      color: "#fff"            
+  },
+  title: 'Login',
+  headerTintColor: "#fff",
+  headerLeft: <Icon name="menu" size={24}
+    iconStyle={{ color: 'white' }} 
+    onPress={ () => navigation.toggleDrawer() } />    
+})
 });
 
 const CustomDrawerContentComponent = (props) => (
